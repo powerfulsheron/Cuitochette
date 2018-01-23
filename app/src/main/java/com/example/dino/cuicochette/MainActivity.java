c8395c617329cbd7e77569c331ea5fd2ba4fdc73
Méryl <meryl.valier@gmail.com>
2018-01-23 10:13:23 +0100

    Page d'accueil + ImageAdapter class

--- app/src/main/java/com/example/dino/cuicochette/MainActivity.java
+++ app/src/main/java/com/example/dino/cuicochette/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.dino.cuicochette;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridView gridView = (GridView)findViewById(R.id.gridview);
+        gridView.setAdapter(new ImageAdapter(this));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Intent myIntent = null;
+                if(position == 0){
+                }
+
+                else if(position == 1){
+                }
+
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
     }
 }

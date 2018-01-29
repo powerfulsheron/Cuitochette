6aff923cd87046152d32574f990db3c6c8c8e23f
Canavaggio Lorenzo <lorenzo.canavaggio@laposte.net>
2018-01-29 09:19:28 +0100

    tg

--- app/src/main/java/com/example/dino/cuicochette/MainActivity.java
+++ app/src/main/java/com/example/dino/cuicochette/MainActivity.java
@@ -1,29 +1,36 @@
 package com.example.dino.cuicochette;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
 
+    DbAdapter helper = new DbAdapter(getApplicationContext());
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
-        GridView gridView = (GridView)findViewById(R.id.gridview);
+        GridView gridView = (GridView) findViewById(R.id.gridview);
         gridView.setAdapter(new ImageAdapter(this));
+
+        Log.i("test",helper.getPlatById(2));
 
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
                 Intent myIntent = null;
-                if(position == 0){
-                }
-
-                else if(position == 1){
+                if (position == 0) {
+                } else if (position == 1) {
                 }
 
                 MainActivity.this.startActivity(myIntent);
